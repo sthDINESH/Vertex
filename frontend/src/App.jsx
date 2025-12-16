747d52b9b61d374fc6c67f8ec1492c217833148f
sthDINESH <sthapit.dnsh@gmail.com>
2025-12-16 16:17:03 +0000

    add local env setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Workflow } from 'lucide-react'
 
 const App = () => {
 
+  const env = import.meta.env.VITE_TEST_ENV
+
   return (
     <>
       <div className="text-3xl font-bold underline">
-        Hello
+        Hello, {env}
       </div>
       <Workflow color='green'/>
     </>

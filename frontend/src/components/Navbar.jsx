0926de30369a182924226a9b46ea5490b316f1d4
sthDINESH <sthapit.dnsh@gmail.com>
2026-01-22 22:25:39 +0000

    add link to homepage for navbar brand

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,9 @@
-import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 
 const Navbar = () => {
 
-  const containerRef = useRef()
   useGSAP(() => {
     gsap.from('nav',{
       yPercent: -100,
@@ -16,11 +14,13 @@ const Navbar = () => {
   },{ dependencies:[] })
 
   return (
-    <nav ref={containerRef} className="fixed top-0 left-0 z-10 flex justify-center w-full bg-linear-to-b to-white/10 backdrop-blur">
+    <nav className="fixed top-0 left-0 z-10 flex justify-center w-full bg-linear-to-b to-white/10 backdrop-blur">
       <div className="container w-full">
         <div className="flex items-center py-2 border-b-2 border-white">
           <div className="nav-brand-container">
-            <div className="accent-text text-white text-xl md:text-2xl lg:text-3xl">Vertex</div>
+            <Link to='/' className="accent-text text-white text-xl md:text-2xl lg:text-3xl hover:opacity-80 transition">
+              Vertex
+            </Link>
           </div>
           <div className="nav-link-container ml-auto">
             <ul className='flex gap-3'>

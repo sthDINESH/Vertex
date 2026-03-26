229e97416b7abd60a12dc56c8bf0f40c90470504
sthDINESH <sthapit.dnsh@gmail.com>
2026-03-26 14:27:25 +0000

    show features section with form only

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from 'react-redux'
 import Hero from '../sections/Hero'
 import UserInteraction from '../sections/UserInteraction'
 import Features from '../sections/Features'
 
 const Home = () => {
+  const mapAvailable = useSelector(state => state.map.status === 'success')
+  const quizAvailable = useSelector(state => state.currentQuiz.status === 'success')
+
+  const showFeaturesSection = !(mapAvailable || quizAvailable)
   return (
     <>
       <header>
@@ -10,7 +15,7 @@ const Home = () => {
       </header>
       <main>
         <UserInteraction />
-        <Features />
+        {showFeaturesSection && <Features />}
       </main>
     </>
   )
